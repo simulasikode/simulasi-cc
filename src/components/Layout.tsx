2c7cd8524fb6c8929ab90320ee70bb4177cf8a8b
simulasikode <simulasikode@gmail.com>
2025-02-01 18:58:47 +0700

    feat:layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -6,7 +6,7 @@ interface LayoutProps {
 
 const Layout = ({ children }: LayoutProps) => {
   return (
-    <div className="max-w-4xl mx-auto p-6">
+    <div className="max-w-4xl mx-auto py-16">
       <main className="mt-6">{children}</main>
     </div>
   );
