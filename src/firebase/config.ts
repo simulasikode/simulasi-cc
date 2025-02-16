f5ef537152d26013c9e54a8607536d24e9b5b977
simulasikode <simulasikode@gmail.com>
2025-02-16 21:09:46 +0700

    fix: dotfile

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBOP3UhZ2B-7naisuTn1JDfqHGbZ8HmzM8",
-  authDomain: "studiosimulasi.firebaseapp.com",
-  projectId: "studiosimulasi",
-  storageBucket: "studiosimulasi.appspot.com",
-  messagingSenderId: "228696722817",
-  appId: "1:228696722817:web:72d572a227fd49fb40ec80",
-  measurementId: "G-2EQYKBXGNE",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
